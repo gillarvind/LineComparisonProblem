777d3c91c95aec7790125b1f64ad7cc26dabd012
Arvind Gill <arvindkhtmuz@gmail.com>
2022-08-23 12:13:05 +0530

    [arvind] Add: folder structure created

---
+++ LineComProblem/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LineComProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LineCompProblem lineCompProblem = new LineCompProblem(1, 2, 3, 4);
+            lineCompProblem.CalculateLength();
+        }
+    }
+}
