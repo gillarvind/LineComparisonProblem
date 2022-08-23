9c654ebaf424571177640e32ef0af2615ce57d4a
Arvind Gill <arvindkhtmuz@gmail.com>
2022-08-23 12:29:06 +0530

    [arvind] Add: line2 branch created

--- LineComProblem/Program.cs
+++ LineComProblem/Program.cs
@@ -5,7 +5,19 @@
         static void Main(string[] args)
         {
             LineCompProblem lineCompProblem = new LineCompProblem(1, 2, 3, 4);
-            lineCompProblem.CalculateLength();
+            double line1= lineCompProblem.CalculateLength();
+            Console.WriteLine("length of the first line"+line1);
+            LineCompProblem lineCompProblem2 = new LineCompProblem(1, 2, 3, 4);
+            double line2 = lineCompProblem.CalculateLength();
+            Console.WriteLine("length of the first line" + line2);
+            if(line1 == line2)
+            {
+                Console.WriteLine("line1 is equal to line2");
+            }
+            else
+            {
+                Console.WriteLine("line1 is not equal to line2");
+            }
         }
     }
 }
